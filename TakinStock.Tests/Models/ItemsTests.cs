bd38df23b9bcdfdbe530f9f6e5bcff010614e663
wfhutch <wfh2727@gmail.com>
2015-12-11 12:02:08 -0600

    Created Unit Tests for Users and Items

---
+++ TakinStock.Tests/Models/ItemsTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TakinStock.Models;
+
+namespace TakinStock.Tests.Models
+{
+    [TestClass]
+    public class ItemsTests
+    {
+        [TestMethod]
+        public void ItemsEnsureICanCreateAClassInstance()
+        {
+            Items item = new Items();
+            Assert.IsNotNull(item);
+        }
+
+        [TestMethod]
+        public void ItemsEnsureICanCreateAnItem()
+        {
+            Items item = new Items();
+            item.ItemsID = 1234;
+            item.Type = "Electronics";
+            item.Make = "Samsung";
+            item.Model = "46HD1234";
+            item.Description = "46 HDTV";
+            item.SerialNumber = "AB123CD456";
+            item.PurchaseDate = "12/1/15";
+            item.PurchasePrice = "499.99";
+            item.PurchasedFrom = "Best Buy";
+            item.Image = "Some S3 URL";
+
+            Assert.AreEqual(item.ItemsID, 1234);
+            Assert.AreEqual(item.Type, "Electronics");
+            Assert.AreEqual(item.Make, "Samsung");
+            Assert.AreEqual(item.Model, "46HD1234");
+            Assert.AreEqual(item.Description, "46 HDTV");
+            Assert.AreEqual(item.SerialNumber, "AB123CD456");
+            Assert.AreEqual(item.PurchaseDate, "12/1/15");
+            Assert.AreEqual(item.PurchasePrice, "499.99");
+            Assert.AreEqual(item.PurchasedFrom, "Best Buy");
+            Assert.AreEqual(item.Image, "Some S3 URL");
+            Assert.IsFalse(item.LostByDamage);
+            Assert.IsFalse(item.Stolen);
+        }
+    }
+}
---
+++ TakinStock.Tests/Models/UserTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TakinStock.Models;
+using System.Collections.Generic;
+
+namespace TakinStock.Tests.Models
+{
+    [TestClass]
+    public class UserTests
+    {
+        [TestMethod]
+        public void UserEnsureICanCreateAClassInstance()
+        {
+            Users user = new Users();
+            Assert.IsNotNull(user);
+        }
+
+        [TestMethod]
+        public void UserEnsureICanCreateEmail()
+        {
+            Users user = new Users();
+            user.Email = "me@me.com";
+            Assert.AreEqual("me@me.com", user.Email);
+        }
+
+        [TestMethod]
+        public void UserEnsureCanSeeAllItems()
+        {
+            List<Items> items_list = new List<Items>
+            {
+                new Items {Make = "Fender" },
+                new Items {Make = "Gretsch" },
+                new Items {Make = "Yamaha" }
+            };
+            Users user = new Users();
+            user.UserID = 1;
+            user.Items = items_list;
+            List<Items> actual = user.Items;
+            CollectionAssert.AreEqual(actual, user.Items);
+        }
+    }
+}
