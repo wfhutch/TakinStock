ea7421a5a87bd5f9538c538310a2e6080f29afb1
wfhutch <wfh2727@gmail.com>
2016-01-08 01:10:22 -0600

    Changed Users class to TakinStockUsers as seen in Owner property

--- TakinStock/Models/Items.cs
+++ TakinStock/Models/Items.cs
@@ -10,7 +10,7 @@ namespace TakinStock.Models
     {
         [Key]
         public int ItemsID { get; set; }
-        public virtual Users Owner { get; set; }
+        public virtual TakinStockUsers Owner { get; set; }
         public string Type { get; set; }
         public string Make { get; set; }
         public string Model { get; set; }
